322e5997daad00160726a00045b5fe77ebfd7a3a
lolzkatXD <93267442+pawarherschel@users.noreply.github.com>
2021-10-28 16:37:01 +0530

    review 2021/10/28

--- src/main/java/com/project/sportsleaguemanagementproject/tournament/MatchTypes.java
+++ src/main/java/com/project/sportsleaguemanagementproject/tournament/MatchTypes.java
@@ -1,7 +1,9 @@
-package com.project.sportsleaguemanagementproject.tournament;
-
-public enum MatchTypes {
-    TEST_MATCH,
-    ONE_DAY_INTERNATIONAL,
-    T_20
-}
+//package com.project.sportsleaguemanagementproject.tournament;
+//
+//public enum MatchTypes {
+//    TEST_MATCH,
+//    ONE_DAY_INTERNATIONAL,
+//    T_20,
+//    T_10, //like t-20 but 10 overs
+//    INDIVIDUAL_MATCH //just 1 match
+//}
